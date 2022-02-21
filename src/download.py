1e75f4cd322396f5e5d7b7a43c722623b0ad1521
sambit-giri <sambitgiri@outlook.com>
2022-02-21 10:28:01 +0100

    preparing for pypi

--- src/download.py
+++ src/download.py
@@ -16,10 +16,10 @@ link_to_emu1_file   = 'https://github.com/sambit-giri/BCemu/releases/download/v1
 link_to_emu1p5_file = 'https://github.com/sambit-giri/BCemu/releases/download/v1.0/kpls_emulator_z1p5_nComp3.pkl'
 link_to_emu2_file   = 'https://github.com/sambit-giri/BCemu/releases/download/v1.0/kpls_emulator_z2_nComp3.pkl'
 
-wget.download(link_to_emu0_file,   path_to_emul_folder)
-wget.download(link_to_emu0p5_file, path_to_emul_folder)
-wget.download(link_to_emu1_file,   path_to_emul_folder)
-wget.download(link_to_emu1p5_file, path_to_emul_folder)
-wget.download(link_to_emu2_file,   path_to_emul_folder)
+print('at z = 0.0'); wget.download(link_to_emu0_file,   path_to_emul_folder)
+print('at z = 0.5'); wget.download(link_to_emu0p5_file, path_to_emul_folder)
+print('at z = 1.0'); wget.download(link_to_emu1_file,   path_to_emul_folder)
+print('at z = 1.5'); wget.download(link_to_emu1p5_file, path_to_emul_folder)
+print('at z = 2.0'); wget.download(link_to_emu2_file,   path_to_emul_folder)
 
 print('...done')
