a46c026d505c4d95690aefa4b332f991b031ee79
sambit-giri <sambitgiri@outlook.com>
2022-02-21 10:09:50 +0100

    preparing for pypi

--- src/download.py
+++ src/download.py
@@ -1,5 +1,7 @@
 import wget
 
+print('Downloading emulators...')
+
 link_to_emu0_file   = 'https://github.com/sambit-giri/BCemu/releases/download/v1.0/kpls_emulator_z0_nComp3.pkl'
 link_to_emu0p5_file = 'https://github.com/sambit-giri/BCemu/releases/download/v1.0/kpls_emulator_z0p5_nComp3.pkl'
 link_to_emu1_file   = 'https://github.com/sambit-giri/BCemu/releases/download/v1.0/kpls_emulator_z1_nComp3.pkl'
@@ -7,3 +9,5 @@ link_to_emu1p5_file = 'https://github.com/sambit-giri/BCemu/releases/download/v1
 link_to_emu2_file   = 'https://github.com/sambit-giri/BCemu/releases/download/v1.0/kpls_emulator_z2_nComp3.pkl'
 
 # wget.download(link_to_emu0_file, './')
+
+print('...done')
