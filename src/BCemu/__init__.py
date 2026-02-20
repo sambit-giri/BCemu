3af216ffcc9185bd38a5cd2a00de174be2eeacfa
sambit-giri <sambit-giri@github.com>
2026-02-20 16:29:56 +0100

    BCemu2025 version 2.0.1 This is a major update to BCemu, which includes the following changes: - We use the baryonification model updated in Schneider et al. (2025). - Redshift is now extended to z=3. - It is a differentiable emulator, which is built using the Flax library. This allows us to compute derivatives of the emulator output with respect to the input parameters, which is useful for parameter inference and optimization. - The network architecture is determined using a hyperparameter optimization procedure, which minimizes the loss over validation data at all redshifts.

--- src/BCemu/__init__.py
+++ src/BCemu/__init__.py
@@ -8,6 +8,15 @@ For example:
 >>> help(BCemu.use_emul)
 '''
 import sys
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("BCemu")
+except importlib.metadata.PackageNotFoundError:
+    # This happens if the user just cloned the repo and imported the folder
+    # without running `pip install .` or `pip install -e .`
+    __version__ = "unknown"
+    
 from .BaryonEffectsEmulator import *
 from . import download
 # from . import kpls 
